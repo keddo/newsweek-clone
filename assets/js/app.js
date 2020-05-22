6a89edaad8d0f0c04332aca4b7208ea7a4962d6e
keddo <adu.kedir.abdurahman@gmail.com>
2020-05-22 14:46:04 +0300

    Navigation bar got responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,19 @@
-$(document).ready(function(){
-  
-});
+window.onscroll = function() { stickToTop()}
+let bottom_nav = document.querySelector('.header-nav-bottom'),
+    header_top = document.querySelector('.header-top'),
+    navbar_logo = document.querySelector('.header-logo'),
+    weather_info = document.querySelector('.weather-info');
+let sticky = bottom_nav.offsetTop;
+
+function stickToTop() {
+    if(window.pageYOffset > sticky) {
+       weather_info.classList.add('d-none');
+       bottom_nav.classList.add('d-none');
+       navbar_logo.classList.add('fixed');
+    }
+    else {
+        weather_info.classList.add('d-none');
+        bottom_nav.classList.add('d-none');
+        navbar_logo.classList.add('fixed');
+    }
+}
