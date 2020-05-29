14f17a6685aaa9c4382b91c5adc6e8be0097e458
keddo <adu.kedir.abdurahman@gmail.com>
2020-05-29 12:20:24 +0300

    Make navbar on scroll fixed top

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,19 @@
-window.onscroll = function() { stickToTop()}
-let bottom_nav = document.querySelector('.header-nav-bottom'),
-    header_top = document.querySelector('.header-top'),
-    navbar_logo = document.querySelector('.header-logo'),
-    weather_info = document.querySelector('.weather-info');
-let sticky = bottom_nav.offsetTop;
 
-function stickToTop() {
-    if(window.pageYOffset > sticky) {
-       weather_info.classList.add('d-none');
-       bottom_nav.classList.add('d-none');
-       navbar_logo.classList.add('fixed');
-    }
-    else {
-        weather_info.classList.add('d-none');
-        bottom_nav.classList.add('d-none');
-        navbar_logo.classList.add('fixed');
-    }
-}
+let distance = $(".header-nav-bottom").offset().top;
+
+$(window).scroll( function() {
+      if($(window).scrollTop() >= distance){
+          $(".weather-info").removeClass("d-lg-flex").addClass('d-lg-none');
+          $(".header-nav-bottom").removeClass('d-lg-block');
+          $(".header-top").addClass('fixed');
+          $(".header-react").removeClass('align-self-end');
+          $(".header-humberger").removeClass('d-lg-none');
+      }
+      else {
+        $(".weather-info").removeClass("d-lg-none").addClass('d-lg-flex');
+        $(".header-nav-bottom").addClass('d-lg-block');
+        $(".header-top").removeClass('fixed');
+        $(".header-react").addClass('align-self-end');
+        $(".header-humberger").addClass('d-lg-none');
+      }
+    });
